b03b7494d897a2da287f920b0d76ce34e2503122
ealpha072 <ealpha072@gmail.com>
2021-01-05 14:19:46 +0300

    New connection to db

--- config.php
+++ config.php
@@ -1,4 +1,14 @@
 <?php
-  
+  #creating a new connection
+
+  $dsn = "mysql:host=localhost;dbname=myDB;charset=utf8mb4";
+  $username = "root";
+  $password = "";
+  try{
+    $conn = new PDO($dsn,$username,$password);
+    echo "Connected successfully!!";
+  }catch(Exceptions $e){
+    echo "Connection failed ".$e->getMessage();
+  }
 
 ?>
