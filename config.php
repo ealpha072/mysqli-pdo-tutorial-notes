46af46d7a2c71b8c5f351b754923ae9d80376ad5
ealpha072 <ealpha072@gmail.com>
2021-01-05 15:24:26 +0300

    PDO::PARAM_STR added to form data received

--- config.php
+++ config.php
@@ -70,9 +70,9 @@
     $stmt = $conn->prepare($sql);
 
     //bind parameters to statement
-    $stmt->bindParam(':first_name', $_REQUEST['first_name']);
-    $stmt->bindParam(':last_name', $_REQUEST['last_name']);
-    $stmt->bindParam(':email', $_REQUEST['email']);
+    $stmt->bindParam(':first_name', $_REQUEST['first_name'],PDO::PARAM_STR);
+    $stmt->bindParam(':last_name', $_REQUEST['last_name'],PDO::PARAM_STR);
+    $stmt->bindParam(':email', $_REQUEST['email'],PDO::PARAM_STR);
 
     //execute the prepired stmt
     $stmt->execute();
@@ -91,6 +91,7 @@
     $stmt->bindParam(':last_name',$last_name,PDO::PARAM_STR);
     $stmt->bindParam(':email',$email,PDO::PARAM_STR);
 
+    //PDO::PARAM_STR tells th system that the incoming bind param is a string
     //set the parameters and execute
     $first_name  ="Hermonie";
     $last_name = "Owen";
