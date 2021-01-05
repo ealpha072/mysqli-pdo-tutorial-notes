9ae5cec691d5aabc241c7a40d593fb44bbbc6d52
ealpha072 <ealpha072@gmail.com>
2021-01-05 14:45:19 +0300

    Creating tables

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
   #creating a new connection
 
-  $dsn = "mysql:host=localhost;dbname=test;charset=utf8mb4";
+  $dsn = "mysql:host=localhost;dbname=demo;charset=utf8mb4";
   $username = "root";
   $password = "";
   try{
@@ -15,12 +15,24 @@
   }
 
   //creating a database
-  try{
+  /*try{
     $sql ="CREATE DATABASE demo";
     $conn->exec($sql);
     echo "Database created successfully";
   }catch(Exceptions $e){
     die("Error: Not able to execute $sql. " .$e->getMessage());
+  }*/
+  try{
+    $sql = "CREATE TABLE persons(
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        email VARCHAR(70) NOT NULL UNIQUE
+    ) ";
+    $conn->exec($sql);
+    echo "Table created successfully!!";
+  }catch(Exceptions $e){
+    die ("Error: Could execute .$sql ".$e->getMessage());
   }
   //closing connection
   $conn = null; //or unset($conn)
