61e17f296247e5ce0fd2e06b19e063ae760c3da6
ealpha072 <ealpha072@gmail.com>
2021-01-05 14:39:44 +0300

    Creating a database

--- config.php
+++ config.php
@@ -8,10 +8,19 @@
     $conn = new PDO($dsn,$username,$password);
     //set PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!!";
+    echo "Connected successfully!!"."<br>";
     //$conn->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
   }catch(Exceptions $e){
     echo "Connection failed ".$e->getMessage();
+  }
+
+  //creating a database
+  try{
+    $sql ="CREATE DATABASE demo";
+    $conn->exec($sql);
+    echo "Database created successfully";
+  }catch(Exceptions $e){
+    die("Error: Not able to execute $sql. " .$e->getMessage());
   }
   //closing connection
   $conn = null; //or unset($conn)
