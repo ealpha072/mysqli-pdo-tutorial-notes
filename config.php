4ea072d6d051c3bee29c0835508a99a034f717d9
ealpha072 <ealpha072@gmail.com>
2021-01-05 14:33:22 +0300

    Closing connection

--- config.php
+++ config.php
@@ -3,14 +3,16 @@
 
   $dsn = "mysql:host=localhost;dbname=test;charset=utf8mb4";
   $username = "root";
-  $password = "yes";
+  $password = "";
   try{
     $conn = new PDO($dsn,$username,$password);
     //set PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully!!";
+    //$conn->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
   }catch(Exceptions $e){
     echo "Connection failed ".$e->getMessage();
   }
-
+  //closing connection
+  $conn = null; //or unset($conn)
 ?>
