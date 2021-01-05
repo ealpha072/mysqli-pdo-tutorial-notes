14e0db2a944a20266d0e5f39e57dbe8fb1b2437a
ealpha072 <ealpha072@gmail.com>
2021-01-05 14:23:45 +0300

    Error handling on fail of connection

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
   #creating a new connection
 
-  $dsn = "mysql:host=localhost;dbname=myDB;charset=utf8mb4";
+  $dsn = "mysql:host=localhost;dbname=test;charset=utf8mb4";
   $username = "root";
-  $password = "";
+  $password = "yes";
   try{
     $conn = new PDO($dsn,$username,$password);
+    //set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully!!";
   }catch(Exceptions $e){
     echo "Connection failed ".$e->getMessage();
